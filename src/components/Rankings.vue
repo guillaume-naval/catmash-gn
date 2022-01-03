a6d5a2ff2870cd4c4800e50ce5f4096b0be7c245
Guillaume NAVAL <navalguillaume@gmail.com>
2022-01-03 12:04:13 +0100

    Ranking css fixes

--- src/components/Rankings.vue
+++ src/components/Rankings.vue
@@ -19,23 +19,25 @@
           <div class="text-h4 pa-10 text-center">
             Top 5 des chats les plus mignons ! 🐱
           </div>
-          <div v-if="cats">
+          <div v-if="cats" class="d-flex justify-center">
             <div
-              class="d-flex justify-center align-center"
+              class="d-flex flex-column justify-center align-center pa-2"
               v-for="(cat, index) in cats.slice(0, 5)"
               :key="cat.id"
             >
-              <div>{{ index + 1 }}</div>
+              <p class="rank">{{ index + 1 }}</p>
               <v-img
                 :src="cat.url"
-                height="200"
-                max-width="200"
+                height="150"
+                max-width="150"
                 alt="image cat"
                 class="rounded-circle"
               >
               </v-img>
-              <div v-if="cat.vote > 1">{{ cat.vote }} votes</div>
-              <div v-else>{{ cat.vote }} vote</div>
+              <p class="font-weight-bold" v-if="cat.vote > 1">
+                {{ cat.vote }} votes
+              </p>
+              <p class="font-weight-bold" v-else>{{ cat.vote }} vote</p>
             </div>
           </div>
         </v-card-text>
@@ -82,4 +84,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.rank {
+  position: relative;
+  top: 50px;
+  right: 50px;
+  z-index: 4;
+  width: 26px;
+  border: 2px solid #666;
+  color: #666;
+  font-weight: bold;
+  text-align: center;
+  background-color: white;
+  border-radius: 50%;
+}
 </style>
